1d3cbe575ad16cb42cf5f6b0175eb493e22521e1
Adam Hundley <adam@fareharbor.com>
2021-07-12 21:35:58 -0600

    add last log out to create users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->dateTime('last_logout', 0);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
