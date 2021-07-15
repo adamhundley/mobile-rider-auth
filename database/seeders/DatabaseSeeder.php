27dbf0b1644531bd834674b159bc7aa9f2a3aef3
Adam Hundley <adam@fareharbor.com>
2021-07-14 22:34:55 -0600

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
+        DB::table('users')->insert([
+            'name' => 'Adam Hundley',
+            'email' => 'adamhundley@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '123',
+            'last_logout' => now(),
+            'remember_token' => Str::random(10),
+        ]);
     }
 }
